a6e78813853b85b6af7327414cf8624d7692d608
AFoo97 <arthur.foozb@gmail.com>
2025-07-10 11:01:05 +1000

    Add HTTP basic authentication for article and comment destruction actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "AFoo97", password: "Pass3134!", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
